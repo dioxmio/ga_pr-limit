9e8b4313643d2a20b7c4adecb4c0aa8a44343e57
Jose Granja <dioxmio@gmail.com>
2022-04-20 18:10:35 +0200

    ✨ testing gql

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ import * as github from '@actions/github';
 
 
 async function run () {
-    console.log('method invoked');
-
     const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-
-    console.log('token');
-    console.log(GITHUB_TOKEN);
-
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context } = github;
     const { pull_request } = context.payload;
 
+    const data = await octokit.graphql(`
+        viewer {
+            repository(name: "lingoda") {
+                pullRequests {
+                    totalCount
+                }
+            }
+        }
+    `)
+
     console.log('finished');
+    console.log(data);
 }
 
 run();
